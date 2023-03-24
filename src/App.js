742f2104e89284be4e6f35de2eebe72f983df2c0
Pedro Mateus <pedro@Pedros-MacBook-Air.local>
2023-03-24 16:41:00 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import React, {useState, useEffect} from 'react';
+
+const App = () => {
+
+  const [character, setCharacter] = useState("1")
+  const [anime, setAnime] = useState("1")
+  const [image, setImage] = useState(<div></div>)
+
+  useEffect(()=> {
+    fetch('https://graphql.anilist.co',{
+        method: "Post",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({
+          query:`
+          query {
+            Character(id: ${anime}) {
+              image {
+                large
+                
+              }
+              name {
+                full
+              }
+              
+            }
+          }
+          
+          
+          
+          
+          `
+        })
+    })
+      .then(response => {
+        return response.json()
+      })
+      .then((data)=> {
+       setCharacter(data)
+       console.log(character);
+      })
+ }, [anime])
+
+function randomNumber(){
+  let number = Math.floor(Math.random() * 100) + 1
+  console.log(anime)
+   setAnime(number)
+   setImage(
+    <div>
+      <h1> {character.data.Character.name.full} </h1>
+      <img src={`${character.data.Character.image.large}`} alt="Character" />
+    </div>
+  )
+} 
+  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1 className='Tittle'>Genarete a random Character</h1>
+      <button onClick={randomNumber} className="button"> Genarete </button>
+      {image}
     </div>
   );
 }
